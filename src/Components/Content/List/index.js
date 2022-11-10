168c4abd5000242fea7129338e76a1cbc29cb6f2
suedacicekli <suedacicekli@gmail.com>
2022-11-10 11:30:33 +0300

    filtering operations

--- src/Components/Content/List/index.js
+++ src/Components/Content/List/index.js
@@ -2,14 +2,24 @@ import React from 'react'
 import { useTodo } from '../../../contexts/ToDoContext';
 import Item from './Item';
 
+let filtered = null;
+
 function List() {
-    const { todos } = useTodo();
-    console.log(todos);
+    const { todos, filter } = useTodo();
+    // console.log(todos);
+
+    filtered = todos;
+
+    if (filter !== 'all') {
+        filtered = todos.filter((todo) => filter === 'active'
+            ? todo.completed === false && todo
+            : todo.completed === true && todo)
+    }
 
     return (
         <ul className="todo-list">
             {
-                todos.map(todo => (
+                filtered.map(todo => (
                     <Item key={todo.id} todo={todo} />
                 ))
             }
--- src/contexts/ToDoContext.js
+++ src/contexts/ToDoContext.js
@@ -4,6 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
+
+    const [filter, setFilter] = useState('all');
     const [todos, setTodos] = useState([
         {
             id: 1,
@@ -44,6 +46,8 @@ export const TodoProvider = ({ children }) => {
         addTodo,
         toggleTodo,
         destroyTodo,
+        filter,
+        setFilter,
     };
 
     return <TodoContext.Provider value={values}>{children}</TodoContext.Provider>
